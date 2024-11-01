98d4e78f2581787bfab16b73cbe086bf415a173d
islam <lameche1islam@gmail.com>
2024-11-01 14:26:48 +0000

    basic starter menu

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-import { Bot } from "grammy";
+import { Bot, InlineKeyboard } from "grammy";
 import { MenuTemplate, MenuMiddleware } from "grammy-inline-menu";
 const dotenv = require("dotenv");
 dotenv.config();
 const token = process.env.BOT_TOKEN;
 const bot = new Bot(token!);
+const keyboard = new InlineKeyboard()
+  .text("Activities", "activities")
+  .text("Join the Team", "join")
+  .text("Social Media", "social")
+  .row()
+  .text("Suggest Improvements", "suggest")
+  .text("About", "about")
+  .text("Discord Server", "discrod")
+  .row()
+  .text("Collaborators", "collaborators");
+bot.command("start", (ctx) => {
+  ctx.reply("Welcome to the bot", {
+    reply_markup: keyboard,
+  });
+});
 bot.on("message", (ctx) =>
   ctx.reply("hello world, your message is " + ctx.message.text)
 );
